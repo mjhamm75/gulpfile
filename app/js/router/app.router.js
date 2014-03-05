8b73d98228e592c777610d7a4029b470c2d6b2cd
Jason Hamm <jason.hamm@availity.com>
2014-03-05 05:32:12 -0500

    Make navbar render in app

--- app/js/router/app.router.js
+++ app/js/router/app.router.js
@@ -4,6 +4,7 @@ define(function(require) {
   'use strict';
 
   var Backbone = require('backbone');
+  var NavView = require('views/navbar.view');
   var TestView = require('views/test.view');
 
   var Router = Backbone.Router.extend({
@@ -11,9 +12,26 @@ define(function(require) {
       "": "list"
     },
 
+    destroyView: function(view) {
+      if(view !== undefined) {
+        view.undelegateEvents();
+        view.$el.removeData().unbind();
+        view.remove();
+        Backbone.View.prototype.remove.call(view);
+      }
+    },
+
     list: function() {
+      var navView = new NavView();
+      $('#nav').html(navView.render().el);
       var testView = new TestView();
       $('#app').html(testView.render().el);
+    },
+
+    swapView: function(view) {
+      this.destroyView(this.currentView);
+      $('#app').html(view.render().el);
+      this.currentView = view;
     }
   });
 
