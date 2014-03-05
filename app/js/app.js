c04b16f63a34d69cdeaae33ae9ddfc6265b921ba
Jason Hamm <jason.hamm@availity.com>
2014-03-05 05:31:55 -0500

    Add bootstrap into app

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,7 @@ define(function(require) {
 
   var Router = require('router/app.router');
   var Backbone = require('backbone');
+  require('bootstrap');
 
   var App = function() {
     this.initialize = function() {
